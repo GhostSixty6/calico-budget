b6741200c038485df344a6e845b522c972cab288
Mitch Stroebel <mitch.sixty6@gmail.com>
2025-04-15 23:00:17 +0200

    feat(calico): added initial frontend mockup

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html class="h-full bg-gray-50" lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Calico Budget</title>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+    <!-- Styles / Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+  </head>
+  <body class="flex items-center lg:justify-center min-h-screen flex-col">
+      {{ $slot }}
+  </body>
+</html>
